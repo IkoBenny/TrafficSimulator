a2102b0c7cea22081ea28bcb212cbd7058ed4aa5
Benny Iko <benny.iko.it@gmail.com>
2023-06-26 00:54:14 -0400

    Refactors Car class. On branch TRAF-6/improvement/car-refactor Changes to be committed: 	modified:   sim/Car.java 	comments unused code segments 	sorts methods by name

--- sim/Car.java
+++ sim/Car.java
@@ -2,23 +2,46 @@ package sim;
 
 import java.awt.geom.Point2D;
 import java.time.Instant;
-import java.util.TimerTask;
 
 public class Car extends Thread {
 	private double currentSpeed;
 	private Point2D position;
 	private Instant lastStop;
 	private boolean isMoving;
-	private volatile boolean wait;
+//	private volatile boolean wait;
 	
 	public Car(Point2D.Double start, double speed) {
 		currentSpeed = speed;
 		position = start;
 		isMoving = false;
 		lastStop = Instant.now();
-		wait = true;
+//		wait = true;
 	}
 
+	public synchronized void calculatePosition() {
+		double now = Instant.now().toEpochMilli() / 1000.0; // milliseconds to seconds
+		double start = lastStop.toEpochMilli() / 1000.0; // milliseconds to seconds
+		double difference = now - start;
+		double distance = difference * getCurrentSpeed();
+		position.setLocation(distance, 0);
+		//System.out.println(toString() + " moving for: " + difference + " seconds..");
+	}
+	
+	/*
+	 * public synchronized void carLock() throws InterruptedException { while(wait)
+	 * { wait(); } }
+	 */
+	
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+	
+	public synchronized String getPositionAsString() {
+		double x = position.getX();
+	    double y = position.getY();
+	    return "(" + x + "," + y + ")";
+	}
+	
 	public void go() {
 		if (isMoving)
 			return;
@@ -32,56 +55,24 @@ public class Car extends Thread {
 					calculatePosition();
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				System.out.println("currentPosition: " + position);
+				//System.out.println("currentPosition: " + position);
 			}
 		}
 	}
 	
-	public synchronized void calculatePosition() {
-			double now = Instant.now().toEpochMilli() / 1000.0; // milliseconds to seconds
-			double start = lastStop.toEpochMilli() / 1000.0; // milliseconds to seconds
-			double difference = now - start;
-			double distance = difference * getCurrentSpeed();
-			position.setLocation(distance, 0);
-			System.out.println(toString() + " moving for: " + difference + " seconds..");
-	}
-	
-	public synchronized void stopCar() {
-		currentSpeed = Constants.STOP_SPEED;
-	}
-
-	public double getCurrentSpeed() {
-		return currentSpeed;
-	}
-
-	public synchronized void setCurrentSpeed(double currentSpeed) {
-			this.currentSpeed = currentSpeed;
-	}
-
 	@Override
 	public void run() {
 		go();
 	}
-
-	public synchronized Point2D getPosition() {
-		return position;
+	
+	public synchronized void setCurrentSpeed(double currentSpeed) {
+		this.currentSpeed = currentSpeed;
 	}
 
-	public synchronized String getPositionAsString() {
-		return position.toString();
+	public synchronized void stopCar() {
+		currentSpeed = Constants.STOP_SPEED;
 	}
-	
-	
-	public synchronized void carLock() throws InterruptedException {
-		while(wait) {
-			wait();
-		}
-	}
-	
-	  public static void main (String [] args) { Car c = new Car(new
-	  Point2D.Double(0.0, 0.0), Constants.START_SPEED); c.run(); }
 	 
 }
