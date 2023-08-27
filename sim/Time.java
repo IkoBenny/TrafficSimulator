0d07c7a8bfcac2dfe3036f5bf086026f8961a811
Benny Iko <benny.iko.it@gmail.com>
2023-08-27 12:33:19 -0400

    changes Time to implement SimObject.

--- sim/Time.java
+++ sim/Time.java
@@ -1,30 +1,16 @@
 package sim;
 
 import java.time.LocalTime;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Time extends TimerTask {
-		public Timer t = new Timer();	//TimerTask requires Timer instance to function
-		private volatile boolean wait;
+public class Time implements SimObject {
 		private String time;
-		
 		private static Logger log = LoggerFactory.getLogger(Time.class);
 		
 		public Time () {
 			log.info("Inside Time()...");
-			wait = true;
 			log.info("Leaving Time()...");
-		}
-		
-		@Override
-		public void run() {
-			log.info("Inside Time.run()...");
-			timestamp();
-			log.info("Leaving Time.run()...");
 		}
 		
 		public String getTime() {
@@ -34,22 +20,26 @@ public class Time extends TimerTask {
 			return time;
 		}
 
-		public synchronized void timestamp() {
+		private synchronized void timestamp() {
 			log.info("Inside Time.timestamp(){}...", time);
 			time =  LocalTime.now().toString();
 			notify();
-			wait = false;
 			log.info("Leaving Time.timestamp()...", time);
 		}
-		
-		public synchronized void clockLock() throws InterruptedException {
-			log.info("Inside Time.clockLock()...");
-			while(wait) {
-				log.info("Inside Time.clockLock() while loop...");
-				wait();
-			}
-			log.info("Leaving Time.clockLock() while loop...");
-			log.info("Leaving Time.clockLock()...");
+
+		@Override
+		public void stop() {
+			
+		}
+
+		@Override
+		public String start() {
+			return getTime();
+		}
+
+		@Override
+		public void pause() {
+			
 		}
 		
 	}
