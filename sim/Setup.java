5ac0f67a68ee92c434b3e99055ce3d670e387af6
Benny Iko <benny.iko.it@gmail.com>
2023-09-04 04:19:49 -0400

    removes build logic from setup object.

--- sim/Setup.java
+++ sim/Setup.java
@@ -7,8 +7,6 @@ import builder.CarBuilderDirector;
 
 public class Setup implements Command {
 	private Mediator mvc;
-	private CarBuilder builder = new CarBuilder();
-	private CarBuilderDirector director = new CarBuilderDirector(builder);
 	
 	Setup(Mediator mvc) {
 		this.mvc = mvc;
@@ -17,8 +15,6 @@ public class Setup implements Command {
 	@Override
 	public void execute() {
 		setup();
-		buildLights();
-		buildCars();
 	}
 
 	private void setup() {
@@ -30,35 +26,5 @@ public class Setup implements Command {
 					mvc.getC().setViewAndModel(mvc.getM(), mvc.getV());
 				}
 			});
-	}
-
-	private void buildLights() {
-		TrafficLight one;
-		TrafficLight two;
-		TrafficLight three;
-		
-		one = mvc.getM().newLight();
-		mvc.getM().addLight(one);
-		two = mvc.getM().newLight();
-		mvc.getM().addLight(two);
-		three = mvc.getM().newLight();	
-		mvc.getM().addLight(three);
-	}
-	
-	private void buildCars() {
-		Car one;
-		Car two;
-		Car three;
-		
-		director.construct();
-		one = builder.getResult();
-		director.construct();
-		two = builder.getResult();
-		director.construct();
-		three = builder.getResult();
-		
-		mvc.getM().addCar(one);
-		mvc.getM().addCar(two);
-		mvc.getM().addCar(three);
 	}
 }
